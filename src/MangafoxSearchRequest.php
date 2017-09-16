f0b5ddd8be33590c39ed91550218463732b33a32
railken <railken@protonmail.ch>
2017-09-16 22:45:01 +0200

    improved MangafoxSearchRequest

--- src/MangafoxSearchRequest.php
+++ src/MangafoxSearchRequest.php
@@ -82,29 +82,43 @@ class MangafoxSearchRequest
 	{
 
 
-		$params = [
-			'name_method' => $this->getRawCommonFilter($builder->getName()->get('filter')),
-			'name' => str_replace("%20", " ", $builder->getName()->get('value')),
-			'type' => $builder->getType(),
-			'author_method' => $this->getRawCommonFilter($builder->getAuthor()->get('filter')),
-			'author' => $builder->getAuthor()->get('value'),
-			'artist_method' => $this->getRawCommonFilter($builder->getArtist()->get('filter')),
-			'artist' => $builder->getArtist()->get('value'),
-			'sort' => $builder->getSortBy()->get('field'),
-			'order' => $builder->getSortBy()->get('direction') == 'desc' ? 'za' : 'az',
-			'advopts' => 1
-		];
+		$params = [];
+
+		$params['advopts'] = 1;
 		
+		# Name 
+		$params['name_method'] = $this->getRawCommonFilter($builder->getName()->get('filter'));
+		$params['name'] = str_replace("%20", " ", $builder->getName()->get('value'));
+
+		# Type 
+		$params['type'] = $builder->getType();
+
+		# Author
+		$params['author_method'] = $this->getRawCommonFilter($builder->getAuthor()->get('filter'));
+		$params['author'] = $builder->getAuthor()->get('value');
+
+		# Artist
+		$params['artist_method'] = $this->getRawCommonFilter($builder->getArtist()->get('filter'));
+		$params['artist'] = $builder->getArtist()->get('value');
+
+		# Sort
+		$params['sort'] = $builder->getSortBy()->get('field');
+		$params['order'] = $builder->getSortBy()->get('direction') == 'desc' ? 'za' : 'az';
+
+		# Genres
 		$params['genres'] = $builder->getGenres()->get('value')->mapWithKeys(function($item) use ($builder){
     		return [$item => $builder->getGenres()->get('filter') == 'include' ? 1 : 2];
 		})->toArray();
 
+		# Released
 		$params['released_method'] = $this->getRawCommonFilter2($builder->getReleasedYear()->get('filter'));
 		$params['released'] = $builder->getReleasedYear()->get('value');
 
+		# Rating
 		$params['rating_method'] = $this->getRawCommonFilter2($builder->getRating()->get('filter'));
 		$params['rating'] = $builder->getRating()->get('value');
 
+		# Is completed?
 		if ($builder->getCompleted() !== null)
 			$params['is_completed'] = $builder->getCompleted() === true ? "1" : "0";
 
