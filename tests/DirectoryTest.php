971e5488f472b42510a3206533ffbdeedc0d1f1a
railken <railken@protonmail.ch>
2017-09-17 17:17:08 +0200

    updated tests

--- tests/DirectoryTest.php
+++ tests/DirectoryTest.php
@@ -23,6 +23,52 @@ class DirectoryTest extends TestCase
         $this->manager = new Mangafox();
     }
 
+    /**
+     * @expectedException Railken\Mangafox\Exceptions\MangafoxDirectoryBuilderInvalidSortByValueException
+     */
+    public function testMangafoxDirectoryBuilderInvalidSortByValueException()
+    {
+        $this->manager->directory()->sortBy('wrong');
+    }
+
+    /**
+     * @expectedException Railken\Mangafox\Exceptions\MangafoxDirectoryBuilderInvalidBrowseByFilterException
+     */
+    public function testMangafoxDirectoryBuilderInvalidBrowseByFilterException()
+    {
+        $this->manager->directory()->browseBy('wrong', 'Action');
+    }
+
+    /**
+     * @expectedException Railken\Mangafox\Exceptions\MangafoxDirectoryBuilderInvalidBrowseByGenreValueException
+     */
+    public function testMangafoxDirectoryBuilderInvalidBrowseByGenreValueException()
+    {
+        $this->manager->directory()->browseBy('genre', 'wrong');
+    }
+    /**
+     * @expectedException Railken\Mangafox\Exceptions\MangafoxDirectoryBuilderInvalidBrowseByStatusValueException
+     */
+    public function testMangafoxDirectoryBuilderInvalidBrowseByStatusValueException()
+    {
+        $this->manager->directory()->browseBy('status', 'wrong');
+    }
+    /**
+     * @expectedException Railken\Mangafox\Exceptions\MangafoxDirectoryBuilderInvalidBrowseByReleasedYearValueException
+     */
+    public function testMangafoxDirectoryBuilderInvalidBrowseByReleasedYearValueException()
+    {
+        $this->manager->directory()->browseBy('released_year', 'wrong');
+    }
+
+    /**
+     * @expectedException Railken\Mangafox\Exceptions\MangafoxDirectoryBuilderInvalidBrowseByInitialValueException
+     */
+    public function testMangafoxDirectoryBuilderInvalidBrowseByInitialValueException()
+    {
+        $this->manager->directory()->browseBy('initial', 'wrong');
+    }
+
 
     public function testBasics()
     {
--- tests/ReleasesTest.php
+++ tests/ReleasesTest.php
@@ -24,6 +24,7 @@ class LatestUpdateTest extends TestCase
     }
 
 
+    
     public function testBasics()
     {
 
