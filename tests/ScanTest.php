1ff4db9f7b1db41d601cc9af71537e763e44f262
railken <railken@protonmail.ch>
2017-09-17 14:17:29 +0200

    cleanup tests

--- tests/ScanTest.php
+++ tests/ScanTest.php
@@ -34,46 +34,12 @@ class ScanTest extends TestCase
     public function testBasics()
     {
 
-        return;
-      
         $manga = $this->manager->resource('one_piece')->get();
 
         $chapter = $manga->volumes->first()->chapters->first();
 
         $scans = $this->manager->scanByUrl($chapter->url)->get();
-        // $scans = $this->manager->scan($manga->uid, $chapter->volume, $chapter->number)->get();
-
-        print_r($scans);
-
-        /*
-        $results = $m
-            ->directory()
-            ->browseBy('genre', 'Action')  # genre|initial_alphabetic|released_year|status
-            ->orderBy('name', 'ASC') # name|popularity|rating|latest_chapter
-            ->page(1)
-            ->get();
-
-        $results = $m
-            ->latestReleases()
-            ->page(1)
-            ->get();
-        */
-
-
-
-        /*
-
-        $manga = $results->first();
-
-        # Retrieve
-
-        $manga = $m->find($manga->getId());
-        $chapter = $maga->getChapters()->first();
-
-
-        $scans = $m->scan($manga, $chapter);
-        */
-
+        $scans = $this->manager->scan($manga->uid, $chapter->volume, $chapter->number)->get();
 
 
     }
